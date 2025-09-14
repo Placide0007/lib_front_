610ba1378df96d1548ff57b7d9080d9cdb05aea2
Placide0007 <lazareplacide02@gmail.com>
2025-09-14 23:31:00 +0200

    update dectaration.d.ts filee

--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -7,5 +7,9 @@ declare module 'typeface-merienda'
 declare module 'typeface-inter'
 declare module 'typeface-saira'
 declare module 'typeface-caveat'
-declare module 'vue-star-rating';
 
+declare module '*.vue' {
+  import { DefineComponent } from 'vue';
+  const component: DefineComponent<{}, {}, any>;
+  export default component;
+}
