aef0f2768243fd435842cab3e83b8d3614fce624
Placide0007 <lazareplacide02@gmail.com>
2025-09-15 00:29:22 +0200

    fix baseUrl

--- src/axios.ts
+++ src/axios.ts
@@ -1,7 +1,7 @@
-import axios from "axios";
+import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://placide-api.railway.app/api'
+  baseURL: 'https://accomplished-reverence.up.railway.app/api'
 });
 
 export default api;
