6b23a85c2cc97cc98d577d13dbc32f845f166162
Placide0007 <lazareplacide02@gmail.com>
2025-09-15 10:21:54 +0200

    update domaine name

--- src/axios.ts
+++ src/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://accomplished-reverence.up.railway.app/api'
+  baseURL: 'https://api-libmanagement-production.up.railway.app/api'
 });
 
 export default api;
